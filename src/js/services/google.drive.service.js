56e1a80bae792092f7f36c17e0448adcdda51010
Guangjun Li <guangjunli@gmail.com>
2015-12-03 14:17:43 -0500

    formatting to make the promise chaining more readable

--- src/js/services/google.drive.service.js
+++ src/js/services/google.drive.service.js
@@ -1,5 +1,5 @@
 angular.module('googleDRRrrRrvrr')
-.factory('googleDriveService', ['$http', '$window', 'googleApiReadyService', function($http, $window, googleApiReadyService) {
+.factory('googleDriveService', ['$http', 'googleApiReadyService', function($http, googleApiReadyService) {
 
   return {
     //save the value here so it's kept when list controller is re-created whenever
@@ -9,45 +9,46 @@ angular.module('googleDRRrrRrvrr')
     listDocuments: function() {
       var that = this;
 
-      return googleApiReadyService.getClient().then(function() {
-        return gapi.client.load('drive', 'v2');
-      }).then(function() {
-        //executes after api client loads
-        var request = gapi.client.drive.files.list({
-            'maxResults': that.maxNumberOfResults,
-            'q': "mimeType = 'application/vnd.google-apps.document'"
-        });
-
-        return request;
-
-      }).then(function(resp) {
+      return googleApiReadyService.getClient()
+        .then(function() {
+          return gapi.client.load('drive', 'v2');
+        })
+        .then(function() {
+          //executes after api client loads
+          return gapi.client.drive.files.list({
+              'maxResults': that.maxNumberOfResults,
+              'q': "mimeType = 'application/vnd.google-apps.document'"
+          });
+        })
+        .then(function(resp) {
           //response for the request
           return resp.result;
-      });
+        });
     },
 
     loadDocument: function(fileId) {
-      return googleApiReadyService.getClient().then(function() {
-        return gapi.client.load('drive', 'v2');
-      }).then(function() {
-        //executes after api client loads
-        var request = gapi.client.drive.files.get({fileId: fileId});
+      return googleApiReadyService.getClient()
+        .then(function() {
+          return gapi.client.load('drive', 'v2');
+        })
+        .then(function() {
+          //executes after api client loads
+          return gapi.client.drive.files.get({fileId: fileId});
+        })
+        .then(function(resp) {
+          var accessToken = gapi.auth.getToken().access_token;
 
-        return request;
-
-      }).then(function(resp) {
-        var accessToken = gapi.auth.getToken().access_token;
-
-        return $http({
-          url: resp.result.exportLinks["text/plain"],
-          method: "GET",
-          headers: {
-            'Authorization': "Bearer " + accessToken
-          }
-        });
-      }).then(function(data) {
+          return $http({
+            url: resp.result.exportLinks["text/plain"],
+            method: "GET",
+            headers: {
+              'Authorization': "Bearer " + accessToken
+            }
+          });
+        })
+        .then(function(data) {
           return data.data;
-      });
+        });
     }
   };
 }]);
